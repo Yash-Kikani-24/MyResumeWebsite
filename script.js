f5203742435d79363e00a32962e6fef26a90ca33
Yash-Kikani-24 <yashkikaniofficial@gmail.com>
2026-03-07 20:19:43 +0530

    Add Inter font files, images, and enhance index.html with new sections and animations
    - Added various font files for the Inter typeface in different styles and sizes.
    - Included new images: MPSTME.avif, ProdDash.png, Waitless.png, and Waitless2.webp.
    - Updated index.html to include new sections for skills, education, services, and projects.
    - Enhanced animations using GSAP for smoother transitions and effects.
    - Improved CSS for better styling and font integration.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger);
 
 gsap.from(".imageanimation",{
     opacity:0,
@@ -5,6 +6,18 @@ gsap.from(".imageanimation",{
     scale:3,
     ease: "power4.in"
 })
+
+gsap.to(".imageanimation", {
+  y: 700,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".coloranimation",
+    start: "top top",
+    end: "bottom top",
+    scrub: true
+  }
+});
+
 gsap.from(".textanimation",{
     x:1000,
     duration:1,
@@ -13,8 +26,8 @@ gsap.from(".textanimation",{
     delay:0.7
 });
 gsap.to(".textanimation",{
-    xPercent:-50,
-    duration:12,
+    xPercent:-100,
+    duration:20,
     ease:"none",
     repeat:-10
 });
@@ -44,3 +57,21 @@ gsap.from(".footeranimation",{
     opacity:0
 })
 
+gsap.to(".togethertexts",{
+    xPercent:-200,
+    duration:15,
+    ease:"none",
+    repeat:-1
+})
+
+gsap.to(".reachoutanim",{
+    xPercent:-100,
+    duration:20,
+    ease:"none",
+    repeat:-10
+})
+
+
+gsap.from(".page2text",{
+    
+})
